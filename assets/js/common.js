ea943bfbadd75d7cce3992488846ede9226f8796
cmq2080 <1421224516@qq.com>
2018-08-07 22:33:56 +0800

    feat: 修改js脚本 (refs: #180807-2233)

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,42 +2,36 @@ function back(){
 	window.history.back(1);
 }
 
-function checkEmail(email){
-	return /^[0-9a-zA-Z]+@[0-9a-zA-Z]+\.[0-9a-zA-Z]+$/.test(email);
-}
-
-function checkTelephone(telephone){
-	return /^1[3|4|5|7|8]{1}[0-9]{9}$/.test(telephone);
-}
-
-function checkNum(obj) {
-	var val=obj.value;
-	while(!/^[0-9]*$/.test(val)){
-		val=val.replace(/[^0-9]+/, '');
-	}
-	obj.value=val;
-}
-
-function checkFloat(obj){
-	var str=obj.value;
-	while(!/^[0-9.]*$/.test(str)){
-		str=str.replace(/[^0-9.]+/, '');
-	}
-	while(!/^[0-9]{1}[0-9.]*/.test(str)){
-		if(str.length==0){obj.value="";return;}
-		str=str.substring(1);
-	}
-	str=str.split(".");
-	if(str.length==1){
-		obj.value= str[0];
-	}else{
-		obj.value= str[0]+"."+str[1];
+function verify(type, val) {
+	switch (type) {
+		case "telephone":
+			return /^1[3|4|5|7|8]{1}[0-9]{9}$/.test(val);
+			break;
+		case "email":
+			return /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(val);
+			break;
+		case "number":
+			if (!val) {
+				return false;
+			} else {
+				return !isNaN(val);
+			}
+			break;
+		case "float":
+			return /^(-?\d+)(\.\d+)?$/.test(val);
+			break;
+		case "date":
+			return /^(\d{4})[-\/](\d{1}|0\d{1}|1[0-2])([-\/](\d{1}|0\d{1}|[1-2][0-9]|3[0-1]))*$/.test(val);
+			break;
+		default:
+			return false;
+			break;
 	}
 }
 
 $(function(){
 	/*
-	 * 前台
+	 * ******************************** 前台 ********************************
 	 */
 	var html_height=parseInt($("html").css("height"));
 	var header_height=$(".header").outerHeight(true);
@@ -49,7 +43,7 @@ $(function(){
 	$(".content").css({"min-height": height+"px"});
 	
 	/*
-	 * 后台
+	 * ******************************** 后台 ********************************
 	 */
 	var e_tr=$(".main-table tbody tr");
 	for(var i=0;i<e_tr.length;i++){
@@ -58,20 +52,21 @@ $(function(){
 		}
 	}
 	
-	$("input[name='id']").on("click", function(){
+	$(".hy-main-table input.id").on("click", function(){
 		if($(this).is(":checked")){
 			$(this).attr("checked", "checked");
 		}else{
 			$(this).removeAttr("checked");
-			$("input[name='dataid']").removeAttr("checked");
+			$(".hy-main-table input.dataid").removeAttr("checked");
 		}
 	});
 	
-	$("input[name='dataid']").on('click', function(){
+	$(".hy-main-table input.dataid").on('click', function(){
+		var e_id=$(".hy-main-table input.id");
 		if($(this).is(":checked")){
-			$("input[name='id']").attr("checked", "checked");
-		}else{
-			$("input[name='id']").removeAttr("checked");
+			$(e_id).prop("checked", "checked");
+		} else {
+			$(e_id).prop("checked", false);
 		}
 	});
 });
